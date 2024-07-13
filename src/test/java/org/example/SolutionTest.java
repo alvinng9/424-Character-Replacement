7f4f80421fc8e6707f6cb71a0fc7b51c9006a29d
Alvin <alvinngg9@gmail.com>
2024-07-13 16:49:46 -0700

    add tests

--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -10,6 +10,9 @@ public class SolutionTest {
         Solution solution = new Solution();
         Assert.assertEquals(4, solution.characterReplacement("ABAB", 2));
         Assert.assertEquals(4, solution.characterReplacement("AABABBA", 1));
+        Assert.assertEquals(8, solution.characterReplacement("ABCBCBCNNCBCBN", 4));
+        Assert.assertEquals(12, solution.characterReplacement("AABBAAAABBBBAAABBBBAAA", 4));
+        Assert.assertEquals(7, solution.characterReplacement("FHHSJJJFHBBBBFFFUENCMAJJSAMCB", 3));
     }
 
 }
